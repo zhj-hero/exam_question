545a087797d530722439da90ff64b2c0fad097c7
zhj-hero <zhj_hero@163.com>
2025-04-09 23:55:27 +0800

    refactor：简化输出

--- 9.打印乘法口诀表.py
+++ 9.打印乘法口诀表.py
@@ -1,6 +1,5 @@
 # 打印乘法口诀表
 for i in range(1,10):
     for j in range(1,i+1):
-        k=i*j
-        print('{i}*{j}={k} '.format(i=i,j=j,k=k),end='')
+        print(f'{i}*{j}={i*j}',end=' ')
     print()
