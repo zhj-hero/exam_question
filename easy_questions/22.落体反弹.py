ac2fe94ee34a0169e3964a7a28e7719591fef38f
zhj-hero <zhj_hero@163.com>
2025-04-16 00:25:12 +0800

    feat: 添加用户输入功能以动态设置球的起始高度和弹跳次数
    在原有代码基础上，增加了从用户输入中读取起始高度和弹跳次数的功能，使用正则表达式解析输入字符串，使程序更具交互性和灵活性。

--- easy_questions/22.落体反弹.py
+++ easy_questions/22.落体反弹.py
@@ -1,37 +1,29 @@
 # 球从100米高度自由落下，每次落地后反跳回原高度的一半;再落下，求它在第10次落地时，共经过多少米?第10次反弹多高?
-
+import re
 
 # 递归函数
-
 def distance(height, n, current=1, total=0):
-
     if current == 1:  # 第一次只有下落
-
         return distance(height/2, n, current+1, total + height)
-
     elif current > n:
         return total, height
-
     else:  # 后续有下落和弹起
-
         return distance(height/2, n, current+1, total + height*2)
 
-
-
 # 迭代函数
-
 def fouction(height, n):
     total = height
     courrent_height = height
-
     for i in range(2, n+1):
         courrent_height /= 2
         total += courrent_height * 2 
     return total, courrent_height / 2
 
+string = input('请输入起始高度和弹跳次数，中间用逗号隔开，如“100,10”:\t')
+lst = re.split(r',', string)
+height = float(lst[0])
+n = int(lst[1])
 
-n = 10
-height = 100
 #递归解法 
 total_distance, final_bounce = distance(height, n)
 print(f'第{n}次落地时，共经过{total_distance:.2f}米，第{n}次反弹{final_bounce:.2f}米。')
